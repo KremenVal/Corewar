59b3404fc409ab250f5438883ee29468a8c4875c
Kostiantyn BAKHARIEV <kbakhari@e1r4p9.unit.ua>
2019-05-28 15:18:53 +0300

    Done?

--- SpongeBug.s
+++ SpongeBug.s
@@ -1,14 +1,14 @@
-.name			""		#comment
-.comment		""		#comment
+.name "Sponge"
+.comment "Sponge"
 						#comment
-live:					#comment
-	live %1				#comment
+live:live%1			#comment
 						#comment
 while:					#comment
-	fork %10 			#comment
+	fork%10 			#comment
 	lfork %10			#comment
-	ld 1, r1			#comment
-	st r1, 50			#comment
+	ld 1, r1
+	aff r78
+	st r1,50
 	ld 0, r1			#comment
 	zjmp %:live			#comment
 						#comment
